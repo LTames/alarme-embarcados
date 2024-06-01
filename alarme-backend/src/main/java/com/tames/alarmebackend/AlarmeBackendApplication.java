64652eacf74cc0722593be56b96770fbe5729a2d
LTames <tamess@protonmail.com>
2024-06-01 19:20:15 -0300

    starting project

---
+++ alarme-backend/src/main/java/com/tames/alarmebackend/AlarmeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tames.alarmebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmeBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlarmeBackendApplication.class, args);
+  }
+
+}
---
+++ alarme-backend/src/test/java/com/tames/alarmebackend/AlarmeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tames.alarmebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
