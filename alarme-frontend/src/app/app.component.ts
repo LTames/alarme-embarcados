47ed69efcd84d47671d5b9054594f8ae4ee14e7d
LTames <tamess@protonmail.com>
2024-06-02 17:14:49 -0300

    feat: add libs frontend

--- alarme-frontend/src/app/app.component.ts
+++ alarme-frontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'alarme-frontend';
